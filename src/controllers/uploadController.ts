7348f2f7f0242f0f00c9cb48c99bc596e5971555
PauloMats <pm.mats98@gmail.com>
2024-08-28 09:18:50 -0300

    Feat: Implementa db, verificação de leitura e salvamento no banco de dados

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 import analyzeImage from '../services/geminiService';
+import knex from 'knex';
+import knexConfig from '../../knexfile';
+
+const db = knex(knexConfig);
 
 const uploadImage = async (req: Request, res: Response) => {
   const { image, customer_code, measure_datetime, measure_type } = req.body;
@@ -31,8 +35,33 @@ const uploadImage = async (req: Request, res: Response) => {
 
   // integração da API Gemini para processar a imagem
   try {
+    // Verificar se já existe uma leitura no mês
+    const existingMeasure = await db('measurements')
+      .where('customer_code', customer_code)
+      .andWhere('measure_type', measure_type)
+      .andWhereRaw('strftime("%Y-%m", measure_datetime) = ?', measure_datetime.substring(0, 7))
+      .first();
 
+    if (existingMeasure) {
+      return res.status(409).json({
+        error_code: "DOUBLE_REPORT",
+        error_description: "Leitura do mês já realizada"
+      });
+    }
+
+    // Integrar com a API Gemini
     const geminiResponse = await analyzeImage(image, measure_type);
+
+    // Salvar no banco de dados
+    await db('measurements').insert({
+      measure_uuid: geminiResponse.measure_uuid,
+      customer_code,
+      measure_datetime,
+      measure_type,
+      has_confirmed: false,
+      image_url: geminiResponse.image_url,
+      measure_value: geminiResponse.measure_value
+    });
 
     // Retornar a resposta da API Gemini
     return res.status(200).json({
@@ -40,13 +69,12 @@ const uploadImage = async (req: Request, res: Response) => {
       measure_value: geminiResponse.measure_value,
       measure_uuid: geminiResponse.measure_uuid
     });
-
   } catch (error) {
     return res.status(500).json({
       error_code: "INTERNAL_SERVER_ERROR",
       error_description: "Erro ao processar a imagem."
     });
   }
+};
 
-}
 export default uploadImage;
